1837cf680ed04ca35fc3962d89d480750a3a90e2
wingmatt <matt@wingwebhelp.com>
2023-04-24 15:53:02 -0700

    Add: Index form layout. Add: Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <svg viewBox="0 0 1rem 1rem" className="playerColor" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="1.1rem" cy="1rem" r="1rem" />
+      </svg>
+      <span>Matt</span>
+    </header>
+  );
+};
+
+export default Header;
