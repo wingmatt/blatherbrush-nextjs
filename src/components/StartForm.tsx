9970d76a9c8fc9d6dcc90d6c939557cdd1b74b79
wingmatt <matt@wingwebhelp.com>
2023-04-27 16:30:11 -0700

    Add: Join lobby page via code. Add: Save name to localStorage

--- src/components/StartForm.tsx
+++ src/components/StartForm.tsx
@@ -1,15 +1,56 @@
-import { useState } from "react";
-import styles from "@/styles/StartForm.module.css"
+import { useEffect, useState } from "react";
+import { NextRouter, useRouter } from "next/router";
+import styles from "@/styles/StartForm.module.css";
+
+const handleSubmit = async (
+  event: React.FormEvent<HTMLFormElement>,
+  form: { player_name: string; lobby_code: string },
+  router: NextRouter
+) => {
+  event.preventDefault();
+
+  router.push({
+    pathname: "play/[lobbyCode]",
+    query: { lobbyCode: form.lobby_code },
+  });
+};
 
 const StartForm = () => {
-  const [form, setForm] = useState({});
+  const [form, setForm] = useState({
+    player_name: "",
+    lobby_code: "",
+  });
+  useEffect(() => {
+    localStorage.setItem("player_name", form.player_name);
+  }, [form.player_name]);
+  const router = useRouter();
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
   return (
-    <form className={styles.start}>
+    <form
+      className={styles.start}
+      onSubmit={(event) => handleSubmit(event, form, router)}
+    >
       <h2>What&apos;s your name?</h2>
-      <input type="text" name="playerName" />
+      <input
+        type="text"
+        name="player_name"
+        value={form.player_name}
+        onChange={(event) => handleChange(event)}
+      />
       <h2>Lobby Code?</h2>
       <div>
-        <input type="text" name="lobbyCode" />
+        <input
+          type="text"
+          name="lobby_code"
+          value={form.lobby_code}
+          maxLength={4}
+          onChange={(event) => handleChange(event)}
+        />
         <button type="submit" id="joinLobby" className="button highlight">
           Join
         </button>
