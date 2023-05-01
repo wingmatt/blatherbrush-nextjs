e659ce656a3678e466cde92cc750a6b52fe7d034
wingmatt <matt@wingwebhelp.com>
2023-04-30 17:00:13 -0700

    Add: Empty scaffold for Prompt component

---
+++ src/components/Prompt.tsx
@@ -0,0 +1,17 @@
+const Prompt = () => {
+  return (
+    // Show obfuscated text if lobby state is suggesting.
+
+    // Show actual prompt text if lobby state is submitting or finished.
+
+    // Text: Show █ for each string array item's length, or the actual text.
+
+    // Blanks: Show underlined blanks, who claimed them
+
+    // Revealing blanks: Set a timeout on a recursive function that looks for the first unfilled blank, reveals it, waits .5s, then shows the next one
+    <>
+    </>
+  );
+}
+
+export default Prompt;
