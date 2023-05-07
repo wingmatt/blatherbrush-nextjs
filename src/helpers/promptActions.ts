6943269aa9818e46b00f046b0ea5483b9005120c
wingmatt <matt@wingwebhelp.com>
2023-05-07 10:57:20 -0700

    Add: Initial attempt at Prompt supabase actions

---
+++ src/helpers/promptActions.ts
@@ -0,0 +1,57 @@
+import { supabase } from "@/helpers/supabaseClient";
+import { Lobby, PromptFragment, Prompt } from "../../types";
+
+// Update a prompt claim: its claim, status, and/or text
+export const updatePrompt = async (lobbyData: Lobby, newPrompt: PromptFragment, newPromptIndex: number): Promise<Lobby> => {
+  lobbyData.prompts[newPromptIndex] = newPrompt;
+  try {
+    let { data, error, status } = await supabase
+      .from("lobby")
+      .update(lobbyData)
+      .eq("code", lobbyData.code);
+
+    if (error && status !== 406) {
+      throw error;
+    }
+
+    if (data) {
+      return data;
+    } else throw error;
+  } catch (error: any) {
+    console.error(error.message);
+    throw error;
+  }
+}
+
+// Select a blank prompt for a new lobby round from an array of possible prompts
+export const randomNewPrompt  = ():Prompt => {
+  const randomIndex = Math.floor(Math.random() * possiblePrompts.length);
+  return possiblePrompts[randomIndex];
+};
+
+const possiblePrompts: Prompt[] = [
+  [
+    "The majestic",
+    {
+      type: "animal",
+      claimed_by: "",
+      status: "open",
+      text: "",
+    },
+    "soared through the sky, its",
+    {
+      type: "adjective",
+      claimed_by: "",
+      status: "open",
+      text: "",
+    },
+    "wings shimmering in the",
+    {
+      type: "time of day",
+      claimed_by: "",
+      status: "open",
+      text: "",
+    },
+    "light.",
+  ],
+];
