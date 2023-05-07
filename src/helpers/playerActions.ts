af8e17e9529fcbcbad8935240e20390e3d0d7218
wingmatt <matt@wingwebhelp.com>
2023-05-07 10:58:08 -0700

    Add: Pseudocode scaffold for playerActions

---
+++ src/helpers/playerActions.ts
@@ -0,0 +1,10 @@
+import { supabase } from "@/helpers/supabaseClient";
+import { Player } from "../../types";
+
+// Get player data from ID
+
+// Create new player
+
+// Update player (name, color) based on ID
+
+// Delete player?
