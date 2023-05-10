af39ff5a8eec17aad934455433f9341a132f5719
wingmatt <matt@wingwebhelp.com>
2023-05-10 16:24:39 -0700

    Change: Move promptSubmission to local state. This will simplify sharing the lobby context between all players

--- src/components/PlayerForm/PlayerForm.tsx
+++ src/components/PlayerForm/PlayerForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import PromptClaim from "./PromptClaim";
 import styles from "@/styles/PlayerForm.module.css";
 import { useUserData } from "@/helpers/UserProvider";
@@ -9,17 +9,7 @@ import { isClaimedPrompt } from "@/helpers/promptActions";
 
 const PlayerForm = () => {
   const {state, dispatch} = useUserData();
-  const [form, setForm] = useState({
-    data: {},
-    promptSubmission: ""
-  });
-  const updatePromptText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setForm(event.target.value);
-    const player_id = state.player.id as string;
-    const claimed_prompt = state.lobby.prompts.find(prompt => isClaimedPrompt(prompt, player_id)) as PromptFragment
-    claimed_prompt.text = event.target.value;
-    dispatch({type: "SET_LOBBY_DATA", payload: state.lobby})
-  };
+  const [promptSubmission, setPromptSumbission] = useState("");
   if (!state.player.id) return <NameForm/>; else return (
     <form className={styles.playerForm}>
       <h2>1 • Claim your prompt!</h2>
@@ -28,7 +18,7 @@ const PlayerForm = () => {
       </div>
       <label>
         <h2>2 • Say something cool!</h2>
-        <input type="text" name="promptSubmission" value={form.promptSubmission} onChange={(event) => updatePromptText(event)}/>
+        <input type="text" name="promptSubmission" value={promptSubmission} onChange={(event: ChangeEvent<HTMLInputElement>) => setPromptSumbission(event.target.value)}/>
       </label>
       <button type="submit" className="button highlight">
         3 • Send it over!
