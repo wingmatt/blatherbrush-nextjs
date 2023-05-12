4e3abc49977ce4683a1a3ee2e4f837fc3e3c4b22
wingmatt <matt@wingwebhelp.com>
2023-05-12 15:27:15 -0700

    Add: compilePrompt helper. Format w/ prettier

--- src/helpers/promptActions.ts
+++ src/helpers/promptActions.ts
@@ -2,7 +2,11 @@ import { supabase } from "@/helpers/supabaseClient";
 import { Lobby, Player, PromptFragment, Prompt } from "../../types";
 
 // Update a prompt claim: its claim, status, and/or text
-export const updatePrompt = async (lobbyData: Lobby, newPrompt: PromptFragment, newPromptIndex: number): Promise<Lobby> => {
+export const updatePrompt = async (
+  lobbyData: Lobby,
+  newPrompt: PromptFragment,
+  newPromptIndex: number
+): Promise<Lobby> => {
   lobbyData.prompts[newPromptIndex] = newPrompt;
   try {
     let { data, error, status } = await supabase
@@ -22,10 +26,10 @@ export const updatePrompt = async (lobbyData: Lobby, newPrompt: PromptFragment, 
     console.error(error.message);
     throw error;
   }
-}
+};
 
 // Select a blank prompt for a new lobby round from an array of possible prompts
-export const randomNewPrompt  = ():Prompt => {
+export const randomNewPrompt = (): Prompt => {
   const randomIndex = Math.floor(Math.random() * possiblePrompts.length);
   return possiblePrompts[randomIndex];
 };
@@ -63,6 +67,25 @@ const possiblePrompts: Prompt[] = [
   ],
 ];
 
-export const isClaimedPrompt = (prompt: PromptFragment | string, player_id: Player["id"]): Boolean => {
-  return (typeof(prompt) == "object" && prompt.status === "claimed" && prompt.claimed_by === player_id)
-}
+export const isClaimedPrompt = (
+  prompt: PromptFragment | string,
+  player_id: Player["id"]
+): Boolean => {
+  return (
+    typeof prompt == "object" &&
+    prompt.status === "claimed" &&
+    prompt.claimed_by === player_id
+  );
+};
+
+export const compilePrompt = (prompts: Prompt): string => {
+  return prompts
+    .reduce((compiledPrompt: string, prompt): string => {
+      typeof prompt !== "string"
+        ? (compiledPrompt += prompt.text)
+        : (compiledPrompt += prompt);
+      compiledPrompt += " ";
+      return compiledPrompt;
+    }, "")
+    .trim();
+};
