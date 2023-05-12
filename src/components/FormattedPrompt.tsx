4d8bed91da911fadaf2293826a36caa5e13a34d4
wingmatt <matt@wingwebhelp.com>
2023-05-12 16:16:09 -0700

    Add: Index as key for FormattedPrompt output

--- src/components/FormattedPrompt.tsx
+++ src/components/FormattedPrompt.tsx
@@ -1,11 +1,11 @@
 import { PromptFragment } from "../../types";
 import { useUserData } from "@/helpers/UserProvider";
 
-const PromptFragmentHtml = (promptFragment: PromptFragment | string) => {
-  if (typeof promptFragment === "string") return <span>{promptFragment}</span>;
+const PromptFragmentHtml = (promptFragment: PromptFragment | string, index: number) => {
+  if (typeof promptFragment === "string") return <span key={index}>{promptFragment}</span>;
   else {
     return (
-      <div className="dynamic">
+      <div className="dynamic" key={index}>
         <span className="claimed_by">{promptFragment.claimed_by}</span>
         <span className="text">{promptFragment.text}</span>
         <span className="type">{promptFragment.type}</span>
@@ -18,7 +18,7 @@ const FormattedPrompt = () => {
   const { state } = useUserData();
   return (
     <aside>
-      {state.lobby.prompts ? state.lobby.prompts.map((promptFragment) => PromptFragmentHtml(promptFragment)) : ""}
+      {state.lobby.prompts ? state.lobby.prompts.map((promptFragment, index) => PromptFragmentHtml(promptFragment, index)) : ""}
     </aside>
   )
 };
