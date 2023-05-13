8a7bbe1fc09125f176c701ed949f6bccc7dc301d
wingmatt <matt@wingwebhelp.com>
2023-05-13 10:49:54 -0700

    Change: Show player color, name in Header. Show "join game" when no player data found

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { useUserData } from "@/helpers/UserProvider";
+
 const Header = () => {
+  const {state} = useUserData()
   return (
     <header>
-      <svg viewBox="0 0 1rem 1rem" className="playerColor" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="1.1rem" cy="1rem" r="1rem" />
+      {state.player.id ? <><svg viewBox="0 0 1rem 1rem" className="playerColor" xmlns="http://www.w3.org/2000/svg">
+        <circle fill={`var(--color-${state.player.color})`} cx="1.1rem" cy="1rem" r="1rem" />
       </svg>
-      <span>Matt</span>
+      <span>{state.player.name}</span></> : <span>Join Game</span>}
     </header>
   );
 };
