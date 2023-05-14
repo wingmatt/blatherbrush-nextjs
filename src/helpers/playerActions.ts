d5cd6e30117b3dcbd76c8ac95e576e8505651f95
wingmatt <matt@wingwebhelp.com>
2023-05-14 15:55:53 -0700

    Add: updatePlayer helper function

--- src/helpers/playerActions.ts
+++ src/helpers/playerActions.ts
@@ -1,5 +1,6 @@
 import { insertAndReturn } from "@/helpers/supabaseClient";
-import { Player } from "../../types";
+import { Lobby, Player, PromptFragment } from "../../types";
+import { updateLobby } from "./lobbyActions";
 
 // Get player data from ID
 
@@ -9,7 +10,7 @@ type createPlayerParams = {
 }
 
 // Create new player
-export const createPlayer = async (player: createPlayerParams) => {
+export const createPlayer = async (player: createPlayerParams): Promise<Player> => {
   const newPlayerData = {
     name: player.name,
     color: player.color
@@ -19,5 +20,15 @@ export const createPlayer = async (player: createPlayerParams) => {
 }
 
 // Update player (name, color) based on ID
+export const updatePlayer = async ( newPlayerData: Player, lobby: Lobby, dispatch: any) => {
+  // Find all prompts claimed by this player and update their claimed_by data
+  const updatedPrompts = lobby.prompts.filter((prompt) => {
+    return (typeof prompt !== 'string' && prompt.claimed_by.id === newPlayerData.id)
+  }) as PromptFragment[]
+  updatedPrompts.forEach(prompt => prompt.claimed_by = newPlayerData)
+  // Send this update, update the lobby data to reflect new player data
+  dispatch({type: "UPDATE_LOBBY_DATA", payload: lobby});
+  await updateLobby(lobby);
+}
 
 // Delete player?
