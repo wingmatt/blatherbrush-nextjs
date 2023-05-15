10ae0b658469301b954107b74e38c9a9824bd7d4
wingmatt <matt@wingwebhelp.com>
2023-05-15 16:17:13 -0700

    Fix: Overeager render of FormattedPrompt

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -17,7 +17,7 @@ const Canvas = () => {
         alt={state.lobby.artUrl ? compilePrompt(state.prompts) : ""}
       />
       {(state.lobby.phase === "generating") ? <h2 className={styles.generatingText}>Our top artbots are &ldquo;painting&rdquo; your commission now!<br/>Here&apos;s the prompt you provided:</h2> : ""}
-      {(state.lobby.phase === "generating" || "finished") ? <FormattedPrompt/> : ""}
+      {(state.lobby.phase === "generating" || state.lobby.phase === "finished") ? <FormattedPrompt/> : ""}
       {(state.lobby.phase === "finished") ? <button className="button" onClick={() => resetLobby(state.lobby.code)}>Play Again</button> : ""}
     </section>
   );
