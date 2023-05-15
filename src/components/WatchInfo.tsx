27ee297b39d7b1c67198a822cdcaeb1cb439d74b
wingmatt <matt@wingwebhelp.com>
2023-05-15 15:58:37 -0700

    Change: /watch content conforms to new data format

---
+++ src/components/WatchInfo.tsx
@@ -0,0 +1,33 @@
+import { useUserData } from "@/helpers/UserProvider";
+import PromptClaim from "./PlayerForm/PromptClaim";
+
+const WatchInfo = () => {
+  const { state } = useUserData();
+  return (
+    <aside>
+      <h2 className="lobby-code">
+        {" "}
+        Lobby Code: <span>{state.lobby.code}</span>
+      </h2>
+      <div role="group" className="">
+        {state.lobby.prompts
+          ? state.lobby.prompts.map((promptClaim, index) =>
+              typeof promptClaim !== "string" ? (
+                <PromptClaim
+                  key={index}
+                  arrayIndex={index}
+                  type={promptClaim.type}
+                  status={promptClaim.status}
+                  claimed_by_id={promptClaim.claimed_by.id}
+                />
+              ) : (
+                ""
+              )
+            )
+          : ""}
+      </div>
+    </aside>
+  );
+};
+
+export default WatchInfo
--- src/pages/watch/[lobbyCode].tsx
+++ src/pages/watch/[lobbyCode].tsx
@@ -1,12 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { Inter } from 'next/font/google'
-import Header from '@/components/Header'
 import Title from '@/components/Title'
 import Canvas from '@/components/Canvas'
-import PromptClaim from "@/components/PlayerForm/PromptClaim";
 import { UserProvider } from '@/helpers/UserProvider'
+import LobbyLayout from '@/components/LobbyLayout'
+import WatchInfo from '@/components/WatchInfo'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,22 +13,11 @@ export default function Home() {
   const lobbyCode = router.query.lobbyCode as string;
   return (
     <UserProvider>
-      <Head>
-        <title>Blatherbrush</title>
-        <meta name="description" content="Make wacky art with your brush buddies!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header/>
-      <main className={` ${inter.className}`}>
-        <Title/>
-        <aside>
-          <h2 className="lobby-code"> Lobby Code: <span>{lobbyCode}</span></h2>
-          <PromptClaims/>
-        </aside>
-        <Canvas/>
-        <p>Waiting for words...</p>
-      </main>
+      <LobbyLayout lobbyCode={router.isReady ? router.query.lobbyCode as string : ''}>
+      <Title/>
+        <WatchInfo/>
+        <Canvas />
+      </LobbyLayout>
     </UserProvider>
   )
 }
