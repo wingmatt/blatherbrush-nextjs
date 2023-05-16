38ba9a4600ef95bcb762c8880839e0ceb84cfafb
wingmatt <matt@wingwebhelp.com>
2023-05-16 15:39:51 -0700

    Change: Display input only if there's a claim. Clear promptSubmission upon submission

--- src/helpers/promptActions.ts
+++ src/helpers/promptActions.ts
@@ -94,6 +94,10 @@ export const isClaimedPrompt = (
   );
 };
 
+export const getClaimedPrompt = (prompts: Prompt, player_id: Player["id"]) => {
+  return prompts.find((prompt) => isClaimedPrompt(prompt, player_id));
+}
+
 export const compilePrompt = (prompts: Prompt): string => {
   return prompts
     .reduce((compiledPrompt: string, prompt): string => {
