b862a7a4b8d749e7ea46f364c2451690acbcbd45
wingmatt <matt@wingwebhelp.com>
2023-05-19 17:18:05 -0700

    Change: Font is now Abel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import { Abel } from 'next/font/google'
 import Title from '@/components/Title'
 import StartForm from '@/components/StartForm'
 
-const inter = Inter({ subsets: ['latin'] })
+const abel = Abel({ subsets: ["latin"], weight: ["400"] });
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={` ${inter.className}`}>
+      <main className={` ${abel.className}`}>
         <Title/>
         <StartForm/>
       </main>
