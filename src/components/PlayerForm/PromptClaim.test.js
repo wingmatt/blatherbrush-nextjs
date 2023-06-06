0a74422206ea1cd513f37bd954587bcf909b17aa
wingmatt <matt@wingwebhelp.com>
2023-06-06 10:29:29 -0700

    WIP: Adjust test to fit new implementation. Going to need to either refactor or overhaul test suite

--- src/components/PlayerForm/PromptClaim.test.js
+++ src/components/PlayerForm/PromptClaim.test.js
@@ -1,21 +1,27 @@
 import { render, screen } from "@testing-library/react";
 import { UserProvider } from "../../helpers/UserProvider";
+import mockProviderState from '../../helpers/__mocks__/mockProviderState.json';
 import userEvent from "@testing-library/user-event";
 import PromptClaim from "./PromptClaim";
 import { supabase } from "../../helpers/supabaseClient";
 jest.mock('../../helpers/supabaseClient');
 
 test("Prompt is selected when clicked", () => {
-  render(<UserProvider><PromptClaim type="noun" claimed_by="me" status="open" /></UserProvider>);
+  jest.fn('../helpers/lobbyActions', () => ({
+    createLobby: jest.fn(mockProviderState.lobby),
+    getLobbyData: jest.fn(mockProviderState.lobby)
+  }));
+  render(<UserProvider><PromptClaim type="noun" claimed_by_id="" status="open" /></UserProvider>);
   const prompt = screen.getByLabelText("noun");
+  const radioSelect = screen.getByRole("radio");
 
   userEvent.click(prompt);
 
-  expect(prompt).toHaveClass("selected");
+  expect(radioSelect).toBeChecked();
 });
 
 test("Prompt has its type as a label", () => {
-  render(<UserProvider><PromptClaim type="noun" claimed_by="me" status="open" /></UserProvider>);
+  render(<UserProvider><PromptClaim type="noun" claimed_by_id="" status="open" /></UserProvider>);
   const prompt = screen.getByLabelText("noun");
 
   expect(prompt).toBeInTheDocument();
