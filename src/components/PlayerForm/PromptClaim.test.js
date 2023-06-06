4904c856b9b3f198a56882333ec537ab216adcaf
wingmatt <matt@wingwebhelp.com>
2023-06-06 10:30:01 -0700

    Remove: Test from older implementation

--- src/components/PlayerForm/PromptClaim.test.js
+++ src/components/PlayerForm/PromptClaim.test.js
@@ -6,19 +6,6 @@ import PromptClaim from "./PromptClaim";
 import { supabase } from "../../helpers/supabaseClient";
 jest.mock('../../helpers/supabaseClient');
 
-test("Prompt is selected when clicked", () => {
-  jest.fn('../helpers/lobbyActions', () => ({
-    createLobby: jest.fn(mockProviderState.lobby),
-    getLobbyData: jest.fn(mockProviderState.lobby)
-  }));
-  render(<UserProvider><PromptClaim type="noun" claimed_by_id="" status="open" /></UserProvider>);
-  const prompt = screen.getByLabelText("noun");
-  const radioSelect = screen.getByRole("radio");
-
-  userEvent.click(prompt);
-
-  expect(radioSelect).toBeChecked();
-});
 
 test("Prompt has its type as a label", () => {
   render(<UserProvider><PromptClaim type="noun" claimed_by_id="" status="open" /></UserProvider>);
