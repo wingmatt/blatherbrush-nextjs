516ecd5dabb4a31082e7fb6383b8cdb9de7f4fb0
wingmatt <matt@wingwebhelp.com>
2023-06-09 14:36:05 -0700

    Format with Prettier

--- src/pages/play/[lobbyCode].tsx
+++ src/pages/play/[lobbyCode].tsx
@@ -1,22 +1,27 @@
-import { useRouter } from 'next/router'
-import Canvas from '@/components/Canvas'
-import PlayerForm from '@/components/PlayerForm/PlayerForm'
-import LobbyLayout from '@/components/LobbyLayout'
+import { useRouter } from "next/router";
+import Canvas from "@/components/Canvas";
+import PlayerForm from "@/components/PlayerForm/PlayerForm";
+import LobbyLayout from "@/components/LobbyLayout";
 
 import { UserProvider } from "@/helpers/UserProvider";
 
 export default function PlayLobby() {
-  const router = useRouter()
-  if (router.isReady) return (
-    <UserProvider>
-      <LobbyLayout lobbyCode={router.isReady ? router.query.lobbyCode as string : ''}>
-        <PlayerForm/>
-        <Canvas displayMode='play' />
-      </LobbyLayout>
-    </UserProvider>
-  ); else return (
-    <UserProvider>
-      <span>loading...!</span>
-    </UserProvider>
-  )
+  const router = useRouter();
+  if (router.isReady)
+    return (
+      <UserProvider>
+        <LobbyLayout
+          lobbyCode={router.isReady ? (router.query.lobbyCode as string) : ""}
+        >
+          <PlayerForm />
+          <Canvas displayMode="play" />
+        </LobbyLayout>
+      </UserProvider>
+    );
+  else
+    return (
+      <UserProvider>
+        <span>loading...!</span>
+      </UserProvider>
+    );
 }
